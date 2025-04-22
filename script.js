00f35d85137d2a43a510104fc55fcbc804fdacf6
sebcourtois <neitsabes974@gmail.com>
2025-04-22 16:31:21 +0200

    feat: item fields editing

--- script.js
+++ script.js
@@ -14,18 +14,19 @@ function makeItemElem(itemData) {
     const item_html = `
 <article class="item" id=${itemData.id}>
     <div class="item-data">
-        <h2>${itemData.label}</h2>
-        <p>${itemData.text}</p>
+        <h2 class="item-field-value" data-editor-name="item_label_editor">${itemData.label}</h2>
+        <input class="item-field-editor hidden" type="text" name="item_label_editor" data-item-field-name="label">
+        <p class="item-field-value" data-editor-name="item_text_editor">${itemData.text}</p>
+        <textarea class="item-field-editor hidden" name="item_text_editor" data-item-field-name="text"
+        rows="4" cols="50"
+        ></textarea>
     </div>
-    <form class="item-editor" hidden>
-        <input class="item-field-editor" type="text" name="item_label_editor">
-        <textarea class="item-field-editor" name="item_text_editor"></textarea>
-    </form>
-    <button class="edit-button" data-item-id="${itemData.id}">Edit</button>
-    <button class="removal-button" data-item-id="${itemData.id}">Delete</button>
-    <button class="move-button" data-item-id="${itemData.id}" data-target-page-index="${targetPageIdx}">
+    <button class="saving-button hidden" data-item-id="${itemData.id}">Save</button>
+    <button class="editing-button" data-item-id="${itemData.id}">Edit</button>
+    <button class="page-move-button" data-item-id="${itemData.id}" data-target-page-index="${targetPageIdx}">
     To Page ${targetPageIdx}
     </button>
+    <button class="removal-button" data-item-id="${itemData.id}">Delete</button>
 </article>
     `.trim();
     const item_template = document.createElement("template");
@@ -39,8 +40,15 @@ function appendItemElem(itemElem, parentElem) {
     const removalButton = itemElem.querySelector(".removal-button");
     removalButton.addEventListener("click", onRemoveButtonClicked);
 
-    const moveButton = itemElem.querySelector(".move-button");
+    const moveButton = itemElem.querySelector(".page-move-button");
     moveButton.addEventListener("click", onMoveButtonClicked);
+
+    const editButton = itemElem.querySelector(".editing-button");
+    editButton.addEventListener("click", onEditButtonClicked);
+
+    const saveButton = itemElem.querySelector(".saving-button");
+    saveButton.addEventListener("click", onSaveButtonClicked);
+
 }
 
 function loadItemsDb() {
@@ -56,7 +64,6 @@ function saveItemsDb() {
 const itemsGroupElem = document.querySelector("#items_group");
 
 const ITEMS_DB = loadItemsDb();
-console.log(ITEMS_DB);
 for (let itemData of ITEMS_DB.values()) {
     if (itemData.page_index !== CURRENT_PAGE_INDEX) continue;
     const itemElem = makeItemElem(itemData);
@@ -90,7 +97,7 @@ function onRemoveButtonClicked(event) {
     removeItemElement(itemId);
 }
 
-function onAddItemClicked(event) {
+function onAddItemClicked() {
     const itemIdx = getItemIndex();
     const itemData = {
         id: `item_${itemIdx}`,
@@ -106,6 +113,66 @@ function onAddItemClicked(event) {
     appendItemElem(itemElem, itemsGroupElem);
 }
 
+function onEditButtonClicked(event) {
+    const editingButton = event.target;
+    const itemId = editingButton.dataset.itemId;
+    const itemElem = document.getElementById(itemId);
+
+    const savingButton = itemElem.querySelector(".saving-button");
+    savingButton.classList.remove("hidden");
+    editingButton.classList.add("hidden");
+
+    const fieldElements = itemElem.querySelectorAll(".item-field-value");
+    for (let fieldElem of fieldElements) {
+        let editorName = fieldElem.getAttribute("data-editor-name");
+        if (editorName === undefined) continue;
+
+        let editorElem = itemElem.querySelector(`[name=${editorName}]`);
+        if (editorElem === null) continue;
+
+        let fieldValue = fieldElem.textContent;
+        if (editorElem instanceof HTMLInputElement) {
+            editorElem.value = fieldValue;
+        } else {
+            editorElem.textContent = fieldValue;
+        }
+        editorElem.classList.remove("hidden");
+        fieldElem.classList.add("hidden");
+    }
+}
+
+function onSaveButtonClicked(event) {
+    const savingButton = event.target;
+    const itemId = savingButton.dataset.itemId;
+    const itemElem = document.getElementById(itemId);
+    const itemData = ITEMS_DB.get(itemId);
+
+    const editorElements = itemElem.querySelectorAll(".item-field-editor");
+    for (let editorElem of editorElements) {
+        let fieldName = editorElem.getAttribute("data-item-field-name");
+        itemData[fieldName] = editorElem.value;
+    }
+    saveItemsDb();
+
+    const fieldElements = itemElem.querySelectorAll(".item-field-value");
+    for (let fieldElem of fieldElements) {
+        let editorName = fieldElem.getAttribute("data-editor-name");
+        if (editorName === undefined) continue;
+
+        let editorElem = itemElem.querySelector(`[name=${editorName}]`);
+        if (editorElem === null) continue;
+
+        let fieldName = editorElem.getAttribute("data-item-field-name");
+        fieldElem.textContent = itemData[fieldName];
+
+        editorElem.classList.add("hidden");
+        fieldElem.classList.remove("hidden");
+    }
+
+    const editingButton = itemElem.querySelector(".editing-button");
+    editingButton.classList.remove("hidden");
+    savingButton.classList.add("hidden");
+}
+
 const addItemBtn = document.querySelector("#add_item_button");
 addItemBtn.addEventListener("click", onAddItemClicked);
-
